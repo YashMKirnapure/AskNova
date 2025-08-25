710e3012d25e14948c32fc14db91b9d6a8695b5a
YashKirnapure77 <yashkirnapure777@gmail.com>
2025-08-25 22:59:53 +0530

    Implemented Card section

--- public/utils.ts
+++ public/utils.ts
@@ -1,6 +1,6 @@
+import { interviewCovers, mappings } from "@/constants";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { mappings } from "@/constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -45,4 +45,3 @@ export const getRandomInterviewCover = () => {
   const randomIndex = Math.floor(Math.random() * interviewCovers.length);
   return `/covers${interviewCovers[randomIndex]}`;
 };
-
