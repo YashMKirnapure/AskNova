fde9335b70807b33871ef067be50aab2d6125f15
YashKirnapure77 <yashkirnapure777@gmail.com>
2025-08-25 09:03:07 +0530

    Auth pages

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Controller,FieldValues,Path,Control } from "react-hook-form";
+import { Input } from "./ui/input";
+import { FormMessage,FormItem,FormLabel, FormControl, FormDescription } from "./ui/form";
 
-const FormField = () => (
-    <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-)
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: 'text' | 'email' | 'password'; 
+}
 
-export default FormField
+const FormField = ({control,name,label,placeholder,type="text"} : FormFieldProps<T>) => {
+    return (<Controller name={name} control={control} render={({field})=>(
+      <FormItem>
+        <FormLabel className="label">{label}</FormLabel>
+        <FormControl>
+          <Input 
+          className="input" 
+          placeholder={placeholder}
+          type={type}
+           {...field} 
+           />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+      )}
+  />
+);
+};
 
-
-
+export default FormField;
